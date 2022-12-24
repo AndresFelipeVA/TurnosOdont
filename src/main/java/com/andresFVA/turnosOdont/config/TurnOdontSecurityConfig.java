702a33ee832d946f729e4fc0c981497211239497
AndresFelipeVA <andresf79@gmail.com>
2022-12-23 19:48:06 -0500

    Migrada la configuración de seguridad del WebSecurityConfigurerAdapter obsoleto al enfoque basado en componentes

--- src/main/java/com/andresFVA/turnosOdont/config/TurnOdontSecurityConfig.java
+++ src/main/java/com/andresFVA/turnosOdont/config/TurnOdontSecurityConfig.java
@@ -1,31 +1,33 @@
 package com.andresFVA.turnosOdont.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import javax.sql.DataSource;
 
 @EnableWebSecurity
-public class TurnOdontSecurityConfig extends WebSecurityConfigurerAdapter {
+public class TurnOdontSecurityConfig {
 
     @Autowired
     private DataSource dataSource;
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
                 .antMatchers("/h2").hasRole("ADMIN")//Habilitar acceso a la consola h2
                 .antMatchers("/dentists/*").hasRole("ADMIN")//Creación, borrado o búsqueda 1 dentist
                 .antMatchers("/patients/*").hasRole("ADMIN")//Creación, borrado o búsqueda 1 patient
-                .antMatchers(HttpMethod.POST,"/api/v1/dentists").hasRole("ADMIN")//API Creación dentist
-                .antMatchers(HttpMethod.POST,"/api/v1/patients").hasRole("ADMIN")//API Creación patient
+                .antMatchers(HttpMethod.POST, "/api/v1/dentists").hasRole("ADMIN")//API Creación dentist
+                .antMatchers(HttpMethod.POST, "/api/v1/patients").hasRole("ADMIN")//API Creación patient
                 .antMatchers("/api/v1/dentists/*").hasRole("ADMIN")//API Borrado o búsqueda 1 dentist
                 .antMatchers("/api/v1/patients/*").hasRole("ADMIN")//API Borrado o búsqueda 1 patient
                 .antMatchers("/images/**").permitAll() //Para poder acceder a las imágenes
@@ -52,17 +54,17 @@ public class TurnOdontSecurityConfig extends WebSecurityConfigurerAdapter {
                 .logoutRequestMatcher(new AntPathRequestMatcher("/perform_logout", "GET"))
                 .invalidateHttpSession(true)
                 .deleteCookies("JSESSIONID");
-                //.logoutUrl("logout")  //Esto lo reemplazamos con el AntPathRequestMatcher
+        //.logoutUrl("logout")  //Esto lo reemplazamos con el AntPathRequestMatcher
+        return http.build();
     }
 
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.jdbcAuthentication()
-                .dataSource(dataSource)
-                .passwordEncoder(myPasswordEncoder());
+    @Bean//De las otras formas funcionaba, pero mantenía el usuario por defecto "user" e imprimía un password aleatorio
+    public UserDetailsManager user(BCryptPasswordEncoder myPasswordEncoder) {//Aunque no sea evidente el uso del encoder
+        return new JdbcUserDetailsManager(dataSource);//asi queda configurado al igual que la DB
     }
 
-    private PasswordEncoder myPasswordEncoder() {
+    @Bean
+    public BCryptPasswordEncoder myPasswordEncoder() {
         return new BCryptPasswordEncoder();
     }
 }
